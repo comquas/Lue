befe338709018cb7d6f2305b8926eccdf7e24e8c
Saturngod <saturngod@gmail.com>
2017-06-26 01:14:19 +0630

    prepare the dashboard

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+
 <div class="container">
     <div class="row">
         <div class="col-md-8 offset-md-2">
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,14 +3,32 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
 
-                <div class="panel-body">
-                    You are logged in!
-                </div>
-            </div>
+
+        <div class="col-md-2">
+            <img src="{{ url('avatars')}}/{{ $user->avatar }}" class="rounded" style="width:140px;">
+        </div>
+        <div class="col-md-10">
+            <b>{{ $user->name }}</b>
+            <div> {{ $user->position->title }}</div>
+        </div>
+    </div>
+    
+    <div class="row">
+        <div class="col-md-2">
+            <ul>
+        <li>3 years in office</li>
+        <li>5 day left for leave</li>
+        <li>2 day left for sick leave</li>
+        </ul>
+        </div>
+        <div class="col-md-10">
+            {{ $user->email }}
+
+            {{ $user->mobile }}
+            {{ $user->location->name }}
+            {{ $user->join_date }}
+            {{ $user->birthday }}
         </div>
     </div>
 </div>
