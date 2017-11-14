40057d0da5e3802120eadbd5082f166f2c10db1b
Saturngod <saturngod@gmail.com>
2017-11-14 10:30:28 +0630

    support SSL

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -14,6 +16,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+        if (env('HTTPS')) {
+            URL::forceScheme('https');
+        }
     }
 
     /**
