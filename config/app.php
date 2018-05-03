5ebeb39448994c4d08122889203d83be8a533858
pymyoswe <lawkanat96@gmail.com>
2018-05-03 23:41:59 +0630

    upgrate laravel 5.6 from 5.5

--- config/app.php
+++ config/app.php
@@ -120,10 +120,6 @@ return [
     |
     */
 
-    'log' => env('APP_LOG', 'single'),
-
-    'log_level' => env('APP_LOG_LEVEL', 'debug'),
-
     /*
     |--------------------------------------------------------------------------
     | Autoloaded Service Providers
