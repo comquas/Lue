21d250aed6d2b465569976ae50c656a2802d16b3
kyawzinphyo <kyawzinphyo@comquas.com>
2018-05-18 09:46:01 +0630

    retrieve data monthly to fullcalendar

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -5,12 +5,16 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\User;
 use App\Leave;
+use Carbon;
 
 class CalendarController extends Controller
 {
      public function calendar(){
-         $leave=Leave::with('user')->get();
-         $user=User::all();
+
+     	 $start=Carbon\Carbon::now()->startOfMonth()->toDateString();
+     	 $end=Carbon\Carbon::now()->endOfMonth()->toDateString();
+         $leave=Leave::whereBetween('from',[$start,$end])->with('user')->get();
+         $user=User::whereBetween('join_date',[$start,$end])->orWhereBetween('birthday',[$start,$end])->get();
          return view('calendar.calendar')->with(['users'=>$user])->with(['leaves'=>$leave]);
 
      }
