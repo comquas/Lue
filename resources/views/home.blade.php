53aeb456bba50f51aca487cd5b9b4c8eaf0bedf7
kyawzin <kzp96.cu@gmail.com>
2018-06-07 11:42:31 +0630

    update home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -43,11 +43,11 @@
             <ul class='user-info-detail'>
             <li><label>Email</label><span>{{ $user->email }}</span></li>
             <li><label>Mobile</label><span>{{ $user->mobile_no }}</span></li>
-<<<<<<< HEAD
+
             <li><label>Address</label><span>{{ optional($user->location)->name }}</span></li>
-=======
+
                 <li><label>Address</label><span>{{ optional( $user->location)->name }}</span></li>
->>>>>>> 0a3ba6917c7e91f5815fc10ca359c900e8011987
+
             <li><label>Join Date</label><span>{{ $user->get_join_date() }}</span></li>
             <li><label>Birthday</label><span>{{ $user->get_birthday() }} ({{ $user->age() }})</span></li>
             </ul>
